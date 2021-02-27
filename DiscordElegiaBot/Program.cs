9d52c8e51a7c32fe27cb7d3faf3e96b5f953d5b9
Vitaliy Shatskiy <kaptowkabest@gmail.com>
2021-02-27 12:32:02 +0200

    init

---
+++ DiscordElegiaBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordElegiaBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
