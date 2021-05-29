4bd5e88466af2618d243c25ba83092595c8cb714
Vitaliy Shatskiy <kaptowkabest@gmail.com>
2021-05-29 23:56:02 +0300

    refractor

--- DiscordElegiaBot/Program.cs
+++ DiscordElegiaBot/Program.cs
@@ -14,7 +14,7 @@ namespace DiscordElegiaBot
         private readonly IConfiguration _config;
         private DiscordSocketClient _client;
 
-        public Program()
+        private Program()
         {
             // create the configuration
             var builder = new ConfigurationBuilder()
@@ -30,28 +30,21 @@ namespace DiscordElegiaBot
             new Program().MainAsync().GetAwaiter().GetResult();
         }
 
-        public async Task MainAsync()
+        private async Task MainAsync()
         {
-            using (var services = ConfigureServices())
-            {
-                var client = services.GetRequiredService<DiscordSocketClient>();
-                _client = client;
-
-
-                client.Log += LogAsync;
-                client.Ready += ReadyAsync;
-                services.GetRequiredService<CommandService>().Log += LogAsync;
-
-                await client.LoginAsync(TokenType.Bot, _config["Token"]);
-                await client.StartAsync();
-
-                await services.GetRequiredService<CommandHandlerService>().InitializeAsync();
-
-                await Task.Delay(-1);
-            }
+            await using var services = ConfigureServices();
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            _client = client;
+            client.Log += LogAsync;
+            client.Ready += ReadyAsync;
+            services.GetRequiredService<CommandService>().Log += LogAsync;
+            await client.LoginAsync(TokenType.Bot, _config["Token"]);
+            await client.StartAsync();
+            await services.GetRequiredService<CommandHandlerService>().InitializeAsync();
+            await Task.Delay(-1);
         }
 
-        private Task LogAsync(LogMessage log)
+        private static Task LogAsync(LogMessage log)
         {
             Console.WriteLine(log.ToString());
             return Task.CompletedTask;
@@ -63,13 +56,8 @@ namespace DiscordElegiaBot
             return Task.CompletedTask;
         }
 
-        // this method handles the ServiceCollection creation/configuration, and builds out the service provider we can call on later
         private ServiceProvider ConfigureServices()
         {
-            // this returns a ServiceProvider that is used later to call for those services
-            // we can add types we have access to here, hence adding the new using statement:
-            // using csharpi.Services;
-            // the config we build is also added, which comes in handy for setting the command prefix!
             return new ServiceCollection()
                 .AddSingleton(_config)
                 .AddSingleton<DiscordSocketClient>()
@@ -77,80 +65,5 @@ namespace DiscordElegiaBot
                 .AddSingleton<CommandHandlerService>()
                 .BuildServiceProvider();
         }
-
-        // public async Task MainAsync()
-        // {
-        //     _client = new DiscordSocketClient();
-        //     _client.MessageReceived += CommandHandler;
-        //     _client.Log += Log;
-        //     var token = await File.ReadAllTextAsync("token.txt");
-        //
-        //     await _client.LoginAsync(TokenType.Bot, token);
-        //     await _client.StartAsync();
-        //     await Task.Delay(-1);
-        // }
-        //
-        // private static Task Log(LogMessage message)
-        // {
-        //     Console.WriteLine(message.ToString());
-        //     return Task.CompletedTask;
-        // }
-
-        // private async Task CommandHandler(SocketMessage message)
-        // {
-        //     if (!message.Content.StartsWith('-'))
-        //         return;
-        //     if (message.Author.IsBot)
-        //         return;
-        //     var command = message.Content.Remove(0, 1).ToLower();
-        //     switch (command)
-        //     {
-        //         case var _ when HelloCommand.Any(str => command.StartsWith(str)):
-        //         {
-        //             await HelloExecutor(message);
-        //             break;
-        //         }
-        //         case var _ when GoodbyeCommand.Any(str => command.StartsWith(str)):
-        //         {
-        //             await GoodbyeExecutor(message);
-        //             break;
-        //         }
-        //         case var _ when RandomImageCommand.Any(str => command.StartsWith(str)):
-        //         {
-        //             await RandomImageExecutor(message, command);
-        //             break;
-        //         }
-        //         case var _ when AccountAgeCommand.Any(s => command.StartsWith(s)):
-        //         {
-        //             await AccountAgeExecutor(message, command, _client);
-        //             break;
-        //         }
-        //         case var _ when CoinFlipCommand.Any(s => command.StartsWith(s)):
-        //         {
-        //             await CoinFlipExecutor(message);
-        //             break;
-        //         }
-        //         case var _ when ServerSiteCommand.Any(s => command.StartsWith(s)):
-        //         {
-        //             await ServerSiteExecutor(message);
-        //             break;
-        //         }
-        //         case var _ when ServersIpCommand.Any(s => command.StartsWith(s)):
-        //         {
-        //             await ServersIpExecutor(message);
-        //             break;
-        //         }
-        //         case var _ when ServerMirageIpCommand.Any(s => command.StartsWith(s)):
-        //         {
-        //             await ServerMirageIpExecutor(message);
-        //             break;
-        //         }
-        //         case var _ when ServerAwpIpCommand.Any(s => command.StartsWith(s)):
-        //         {
-        //             await ServerAwpIpExecutor(message);
-        //             break;
-        //         }
-        //     }
-        // }
     }
 }
